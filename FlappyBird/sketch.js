663c7a173e935f1ceef180967a5971061b185d22
necronsilvan <52135298+necronsilvan@users.noreply.github.com>
2019-08-11 17:00:50 +0900

    Add files via upload

--- FlappyBird/sketch.js
+++ FlappyBird/sketch.js
@@ -1,46 +1,85 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/cXgA1d_E-jY&
-
-
-var bird;
+var ovjBird;
 var pipes = [];
 var play = true;
+var score = 0;
+var LiveScore = 0;
+var bird, over, sky;
+
+function preload()
+{
+  // load images
+  sky = loadImage("sky.png");
+  bird = loadImage("bird.png");
+  over = loadImage("over.png");
+}
 
 function setup() {
-  createCanvas(640, 480);
-  bird = new Bird();
-  pipes.push(new Pipe());
+    createCanvas(400,600);
+    ovjBird = new ovjBird();
+    pipes.push(new pipe());
 }
 
 function draw() {
-  background(0);
+    background(0);
+    if (!play) {
 
-  for (var i = pipes.length-1; i >= 0; i--) {
-    pipes[i].show();
-    pipes[i].update();
+        textSize(40);
+        fill(255, 0, 0);
+        textAlign(CENTER, CENTER);
+        strokeWeight(1);
+        stroke(155, 200, 200);
+        text('Game Over',width/2, height/2);
+        imageMode(CENTER);
+        image(over, width/2, height/2-60, 60, 60);
+        textSize(20);
+        fill(255, 210);
+        text('Your LiveScore is '+LiveScore+'!', width/2, 50+height/2);
+        textSize(14);
+        fill(255, 180);
+        text('Press F5 Restart', width/2, 100+height/2);
+        }
+    else {
+          // display background image
+        image(sky, 0, 0);
+        strokeWeight(1);
+        stroke(100);
 
-    if (pipes[i].hits(bird)) {
-      console.log("HIT");
+        for (var i = pipes.length-1; i >= 0; i--) {
+        pipes[i].show();
+        pipes[i].update();
+
+        if (pipes[i].hits(ovjBird)) {
+            console.log("HIT");
+            score -= 3;
+
+            if(score < 0) {
+                play = false;
+            }
+        }
+
+        if (pipes[i].offscreen()) {
+            pipes.splice(i, 1);
+            }
+        }
+            ovjBird.update();
+            ovjBird.show();
+
+        if (frameCount % 70 == 0) {
+            pipes.push(new pipe());
+        }
+            if (frameCount % 100 == 0) {
+                score+= 1;
+                LiveScore+=1;
+            }
+        textSize(24);
+        fill(255);
+        strokeWeight(4);
+        stroke(50, 100, 255);
+        text('HP : '+score, 20, 30);
     }
-
-    if (pipes[i].offscreen()) {
-      pipes.splice(i, 1);
-    }
-  }
-
-  bird.update();
-  bird.show();
-
-  if (frameCount % 75 == 0) {
-    pipes.push(new Pipe());
-  }
+}
+function touchStarted() {
+    ovjBird.up();
 }
 
-function keyPressed() {
-  if (key == ' ') {
-    bird.up();
-    //console.log("SPACE");
-  }
-}
+
